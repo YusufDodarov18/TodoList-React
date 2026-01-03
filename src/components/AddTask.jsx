8482af4e57fc1535e41ae22d0e5250245e188e57
Yusuf Dodarov <dodarovyusuf77@gmail.com>
2026-01-04 06:42:42 +1100

    Update AddTask.jsx

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,9 +1,9 @@
-import css from "../Styles/AddTask.css"
+import styles from "../Styles/AddTask.module.css";
 function AddTask({input,setInput,setData}) {
   return (
     <div>
-        <input type="text" className="inputAdd" value={input} onChange={(ev)=>setInput(ev.target.value)} />
-        <button className="btnAdd" onClick={()=>{
+        <input type="text"  className={styles.inputAdd}  value={input} onChange={(ev)=>setInput(ev.target.value)} />
+        <button className={styles.btnAdd}  onClick={()=>{
           if(input.trim()!==""){
             setData(prevData=>[...prevData,{id:Date.now(),title:input,completed:false}])
             setInput("")
