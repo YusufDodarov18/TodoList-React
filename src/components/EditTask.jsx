6dfc79a4f30d8467b87e92ec96051831013d2623
Yusuf Dodarov <dodarovyusuf77@gmail.com>
2026-01-04 06:48:04 +1100

    Update EditTask.jsx

--- src/components/EditTask.jsx
+++ src/components/EditTask.jsx
@@ -1,4 +1,4 @@
-import css from "../Styles/Edit.css"
+import styles from "../Styles/Edit.module.css"
 import Dialog from './Dialog'
 import { useRef } from 'react'
 
@@ -6,7 +6,7 @@ export const EditTask=({id,data,setData})=> {
     const dialog=useRef(null)
   return (
     <div>
-        <button className="edit" onClick={()=>dialog.current.showModal()}>Edit</button>
+        <button className={styles.edit} onClick={()=>dialog.current.showModal()}>Edit</button>
         <Dialog data={data} setData={setData} id={id} dialog={dialog}/>
     </div>
   )
