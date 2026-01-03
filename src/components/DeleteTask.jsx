da0bf98e248ff687bc77b8dfd3fdb764969c3e88
Yusuf Dodarov <dodarovyusuf77@gmail.com>
2026-01-04 06:46:06 +1100

    Update DeleteTask.jsx

--- src/components/DeleteTask.jsx
+++ src/components/DeleteTask.jsx
@@ -1,8 +1,8 @@
-import css from "../Styles/Delete.module.css"
+import styles from "../Styles/Delete.module.css"
 export const DeleteTask=({id,data,setData})=> {
   return (
     <div>
-        <button className="delete" onClick={()=>setData(data.filter(e=>e.id!==id))}>Delete</button>
+        <button className={styles.delete} onClick={()=>setData(data.filter(e=>e.id!==id))}>Delete</button>
     </div>
   )
 }
